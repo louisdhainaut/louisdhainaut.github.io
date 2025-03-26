61510e13f3a658175286c2e93c189d34d0257fed
bacomus <150095707+bacomus@users.noreply.github.com>
2025-03-26 16:16:48 +0100

    Creation fichier + rangement

--- js/infinite.js
+++ js/infinite.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let score = 0;
 
     function afficherImageAleatoire() {
-        fetch("images/images.json")
+        fetch("img/images.json")
             .then(response => response.json())
             .then(data => {
                 const randomItem = data[Math.floor(Math.random() * data.length)];
